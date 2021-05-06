7b743697466467325da0a20388ef8b9976d74fd3
Abdelghani Moussaid <moussaidabdelghani@gmail.com>
2021-05-06 11:48:17 +0000

    Default route showing active listings

--- commerce/settings.py
+++ commerce/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static/auctions')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
